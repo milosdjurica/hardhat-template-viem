0101ee85cc7547abd3803a1ac05f530184317cf3
milosdjurica <milosdjurica13@gmail.com>
2024-05-02 19:38:28 +0200

    hardhat config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,12 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "dotenv/config";
 
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+
 const config: HardhatUserConfig = {
   // solidity: "0.8.20",
   solidity: {
@@ -23,13 +29,40 @@ const config: HardhatUserConfig = {
       // allowUnlimitedContractSize: true,
       // blockConfirmations: 3
     },
-    localhost: {},
-    ganache: {},
-    sepolia: {},
+    localhost: {
+      chainId: 1337,
+      url: "http://127.0.0.1:7545",
+    },
+    ganache: {
+      chainId: 1337,
+      url: "http://127.0.0.1:7545",
+    },
+    sepolia: {
+      chainId: 11155111,
+      // url: SEPOLIA_RPC_URL,
+      // accounts: [PRIVATE_KEY],
+    },
   },
-  etherscan: {},
-  gasReporter: {},
-  mocha: {},
+  etherscan: {
+    apiKey: {
+      // mainnet: ETHERSCAN_API_KEY,
+      sepolia: ETHERSCAN_API_KEY,
+      // polygon: POLYGONSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    // put it enabled: true -> only when you want to check gas optimizations
+    enabled: false,
+    noColors: true,
+    outputFile: "gas-report.txt",
+    currency: "USD",
+    excludeContracts: [],
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: "MATIC", // polygon network
+  },
+  mocha: {
+    timeout: 500000, //500 seconds
+  },
 };
 
 export default config;
