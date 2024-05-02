8a20ab064b00bd28f2ce2fb1f3aaa5b729862443
milosdjurica <milosdjurica13@gmail.com>
2024-05-02 15:53:45 +0200

    dotenv package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
 };
 
 export default config;
+
