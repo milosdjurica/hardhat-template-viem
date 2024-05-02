f7a25a60dd155d60c7b077f3567dfdd2898254ef
milosdjurica <milosdjurica13@gmail.com>
2024-05-02 16:07:33 +0200

    hardhat config set up starting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,33 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  // solidity: "0.8.20",
+  solidity: {
+    compilers: [{ version: "0.8.20" }],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31137,
+      // forking: {
+      // 	url: MAINNET_RPC_URL,
+      // },
+      // allowUnlimitedContractSize: true,
+      // blockConfirmations: 3
+    },
+    localhost: {},
+    ganache: {},
+    sepolia: {},
+  },
+  etherscan: {},
+  gasReporter: {},
+  mocha: {},
 };
 
 export default config;
